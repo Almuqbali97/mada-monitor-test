b23bbf3b0bd2b71cb4f7806bed30acb6cee91cd9
Almuqblai97 <almuqbalimusab@gmail.com>
2025-01-07 14:19:59 +0400

    add new service monitoring

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ app.get("/send-telemetry", async (req, res) => {
     const sqlService = await getZabbixData(
       "zabbix_get -s 127.0.0.1 -p 10050 -k proc.cpu.util[,mysql]"
     );
+    const zabbixServer = await getZabbixData(
+      "zabbix_get -s 127.0.0.1 -p 10050 -k proc.cpu.util[,zabbix]"
+    );
     console.log(cpuLoad);
     const response = await fetch(thingsboardUrl, {
       method: "POST",
@@ -35,7 +38,8 @@ app.get("/send-telemetry", async (req, res) => {
         cpuLoad: cpuLoad,
         memory: memory,
         storage: storage,
-        SQL_ervice: sqlService,
+        SQL_service: sqlService,
+        zabbix: zabbixServer,
       }),
     });
 
@@ -51,52 +55,54 @@ app.get("/send-telemetry", async (req, res) => {
   }
 });
 
-
-
 // Function to send telemetry
 async function sendTelemetry() {
-    try {
-      const cpuLoad = await getZabbixData(
-        "zabbix_get -s 127.0.0.1 -p 10050 -k proc.cpu.util"
-      );
-      const memory = await getZabbixData(
-        "zabbix_get -s 127.0.0.1 -p 10050 -k vm.memory.size[pused]" // Memory usage in percentage
-      );
-      const storage = await getZabbixData(
-        "zabbix_get -s 127.0.0.1 -p 10050 -k vfs.fs.size[/,pused]" // Storage usage of root filesystem in percentage
-      );
-      const sqlService = await getZabbixData(
-        "zabbix_get -s 127.0.0.1 -p 10050 -k proc.cpu.util[,mysql]"
-      );
-  
-      const telemetryData = {
-        cpuLoad: cpuLoad,
-        memory: memory,
-        storage: storage,
-        SQL_Service: sqlService,
-      };
-  
-      const response = await fetch(thingsboardUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(telemetryData),
-      });
-  
-      if (response.ok) {
-        console.log("Telemetry sent successfully:", telemetryData);
-      } else {
-        console.error("Failed to send telemetry:", response.statusText);
-      }
-    } catch (error) {
-      console.error("Error sending telemetry:", error.message);
+  try {
+    const cpuLoad = await getZabbixData(
+      "zabbix_get -s 127.0.0.1 -p 10050 -k proc.cpu.util"
+    );
+    const memory = await getZabbixData(
+      "zabbix_get -s 127.0.0.1 -p 10050 -k vm.memory.size[pused]" // Memory usage in percentage
+    );
+    const storage = await getZabbixData(
+      "zabbix_get -s 127.0.0.1 -p 10050 -k vfs.fs.size[/,pused]" // Storage usage of root filesystem in percentage
+    );
+    const sqlService = await getZabbixData(
+      "zabbix_get -s 127.0.0.1 -p 10050 -k proc.cpu.util[,mysql]"
+    );
+    const zabbixServer = await getZabbixData(
+      "zabbix_get -s 127.0.0.1 -p 10050 -k proc.cpu.util[,zabbix]"
+    );
+
+    const telemetryData = {
+      cpuLoad: cpuLoad,
+      memory: memory,
+      storage: storage,
+      SQL_Service: sqlService,
+      zabbix_Service: zabbixServer,
+    };
+
+    const response = await fetch(thingsboardUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(telemetryData),
+    });
+
+    if (response.ok) {
+      console.log("Telemetry sent successfully:", telemetryData);
+    } else {
+      console.error("Failed to send telemetry:", response.statusText);
     }
+  } catch (error) {
+    console.error("Error sending telemetry:", error.message);
   }
-  
-  // Trigger telemetry every 3 seconds
-  setInterval(sendTelemetry, 3000);
-  
+}
+
+// Trigger telemetry every 3 seconds
+setInterval(sendTelemetry, 3000);
+
 // Function to get data from Zabbix agent
 function getZabbixData(command) {
   return new Promise((resolve, reject) => {
